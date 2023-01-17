2f19e0e7210b3bb3ce0631d66ce7f8f49284eab9
adamgayoso <adamgayoso@users.noreply.github.com>
2023-01-17 13:11:55 -0800

    move things over

--- docs/api.md
+++ docs/api.md
@@ -1,38 +1,16 @@
 # API
 
-## Preprocessing
+## Core
 
 ```{eval-rst}
-.. module:: simple_scvi.pp
+.. module:: simple_scvi
 .. currentmodule:: simple_scvi
 
 .. autosummary::
     :toctree: generated
 
-    pp.basic_preproc
-```
-
-## Tools
-
-```{eval-rst}
-.. module:: simple_scvi.tl
-.. currentmodule:: simple_scvi
-
-.. autosummary::
-    :toctree: generated
-
-    tl.basic_tool
-```
-
-## Plotting
-
-```{eval-rst}
-.. module:: simple_scvi.pl
-.. currentmodule:: simple_scvi
-
-.. autosummary::
-    :toctree: generated
-
-    pl.basic_plot
-    pl.BasicClass
+    MyModel
+    MyPyroModel
+    MyModule
+    MyPyroModule
 ```
--- src/simple_scvi/__init__.py
+++ src/simple_scvi/__init__.py
@@ -1,7 +1,25 @@
+import logging
 from importlib.metadata import version
 
-from . import pl, pp, tl
+from rich.console import Console
+from rich.logging import RichHandler
 
-__all__ = ["pl", "pp", "tl"]
+logger = logging.getLogger(__name__)
+# set the logging level
+logger.setLevel(logging.INFO)
+
+# nice logging outputs
+console = Console(force_terminal=True)
+if console.is_jupyter is True:
+    console.is_jupyter = False
+ch = RichHandler(show_path=False, console=console, show_time=False)
+formatter = logging.Formatter("mypackage: %(message)s")
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+# this prevents double outputs
+logger.propagate = False
+
+__all__ = ["MyModel", "MyModule", "MyPyroModel", "MyPyroModule"]
 
 __version__ = version("simple-scvi")
