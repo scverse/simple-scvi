6e2af1d01f63633eeaefc280002379038ef72fa9
adamgayoso <adamgayoso@users.noreply.github.com>
2023-01-17 13:17:47 -0800

    imports

--- src/simple_scvi/__init__.py
+++ src/simple_scvi/__init__.py
@@ -4,6 +4,9 @@ from importlib.metadata import version
 from rich.console import Console
 from rich.logging import RichHandler
 
+from ._mymodel import MyModel, MyModule
+from ._mypyromodel import MyPyroModel, MyPyroModule
+
 logger = logging.getLogger(__name__)
 # set the logging level
 logger.setLevel(logging.INFO)
@@ -13,7 +16,7 @@ console = Console(force_terminal=True)
 if console.is_jupyter is True:
     console.is_jupyter = False
 ch = RichHandler(show_path=False, console=console, show_time=False)
-formatter = logging.Formatter("mypackage: %(message)s")
+formatter = logging.Formatter("simple_scvi: %(message)s")
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
